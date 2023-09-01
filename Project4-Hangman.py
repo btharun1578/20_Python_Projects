3a2ab3e8da6fc33707c182cee9ff0713f56aeff0
btharun1578 <135518885+btharun1578@users.noreply.github.com>
2023-09-01 10:44:25 +0530

    Add files via upload
    User can choose more than one word at a time

--- Project4-Hangman.py
+++ Project4-Hangman.py
@@ -8,6 +8,7 @@ def run_game():
 
     guessed_chars=""
     tries=3
+    cheat=1
 
     while tries>0:
         blanks=0
@@ -23,9 +24,18 @@ def run_game():
             print("You got it")
             break
         guess_the_letter=input("Enter a letter: ")
+        if(len(guess_the_letter)>1):
+            if(cheat==1):
+                print("U r trying cheat...Final Warning")
+                cheat-=1
+                continue
+            else:
+                print("You have cheated more than once..You are out od the game")
+                break
         if(guess_the_letter in guessed_chars):
             print("You have used the letter....dont waste your time")
             continue
+        #adding the letter to the previous guessed letter
         guessed_chars+=guess_the_letter
         if guess_the_letter not in word:
             tries-=1
