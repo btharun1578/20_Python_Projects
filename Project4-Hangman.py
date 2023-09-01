6f7615493a2063747e7ff1fae7cb80919ca3aeee
btharun1578 <135518885+btharun1578@users.noreply.github.com>
2023-09-01 10:22:51 +0530

    Add files via upload

---
+++ Project4-Hangman.py
@@ -0,0 +1,45 @@
+from random import choice
+
+def run_game():
+    word=choice(["hello","name","Tharun"])
+
+    name=input("Enter your name:")
+    print(f"welcome to the game Hangman: {name}")
+
+    guessed_chars=""
+    tries=3
+
+    while tries>0:
+        blanks=0
+        print("Word: ",end="")
+        for char in word:
+            if char in guessed_chars:
+                print(char,end="")
+            else:
+                print("_",end="")
+                blanks+=1
+        print()
+        if(blanks==0):
+            print("You got it")
+            break
+        guess_the_letter=input("Enter a letter: ")
+        if(guess_the_letter in guessed_chars):
+            print("You have used the letter....dont waste your time")
+            continue
+        guessed_chars+=guess_the_letter
+        if guess_the_letter not in word:
+            tries-=1
+            print("You have guessed wrong letter...{tries} tries remaining")
+
+            if tries==0:
+                print("Sorry...You have Lost!")
+                break
+
+
+
+
+
+
+
+if __name__=="__main__" :   
+    run_game()
